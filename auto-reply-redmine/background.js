002cfb560fc25499dc4c7dd2bfa14c27b1351e4a
pin <elliephc@gmail.com>
2019-04-16 16:12:06 +0800

    auto select '已回應' works

--- auto-reply-redmine/background.js
+++ auto-reply-redmine/background.js
@@ -1,1 +1,4 @@
- $("body").append('Test');
+$("body").append('Testttt');
+document.querySelector('#issue_status_id').value = '4';
+// $('#issue_assigned_to_id option[text="wilson"]').attr("selected","selected");
+document.querySelector('#issue_notes').content = 'hi';
---
+++ auto-reply-redmine/popup.js
@@ -0,0 +1,9 @@
+// function clickHandler(e) {
+//     // chrome.tabs.update({url: "http://52.192.182.113:8080/issues/6919/edit"});
+//     window.close(); // Note: window.close(), not this.close()
+//     document.querySelector('#issue_status_id').value = '4';
+    
+// }
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('click-me').addEventListener('click', clickHandler);
+// });
