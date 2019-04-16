f5f02935a69c0328a2c7bf2edd40370da40ae97b
pin <elliephc@gmail.com>
2019-04-16 17:46:00 +0800

    nothing

--- auto-reply-redmine/background.js
+++ auto-reply-redmine/background.js
@@ -1,13 +1,11 @@
 	$("#update").show();
-	$("body").append('Testttt11');
-	// $("#issue_status_id").val = '5';
+	// $("body").append('Testttt11');
 	$('#issue_status_id option[value="4"]').attr('selected', 'selected');
 	$('#issue_assigned_to_id option:contains("wilson")').attr('selected', 'selected');
-	// $("#issue_assigned_to_id").val = '63';
-	// $('#issue_assigned_to_id option[text="wilson"]').attr("selected","selected");
 	$("#issue_notes").val('Hi' + "\n" +'已完成 請確認 謝謝');
-	// $("#issue-form").submit();
-	/* Do some initialization */
-	// $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+	
+	// $("#issue-form").submit(); //submit will cause endless loop
 
-	$("html, body").scrollTop($(document).height()-50);
+	// $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+console.log($(document).height());
+	$("html, body").scrollTop($(document).height());
