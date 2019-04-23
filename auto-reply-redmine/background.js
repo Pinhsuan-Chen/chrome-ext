cb96a2200d43d0d46a390cdc152ad5c2e3d031cc
pin <elliephc@gmail.com>
2019-04-23 10:34:52 +0800

    ??

--- auto-reply-redmine/background.js
+++ auto-reply-redmine/background.js
@@ -2,8 +2,8 @@ var assigner = $(".has-notes:last a.user").html()
 $("#update").show();
 	// $("body").append('Testttt11');
 	$('#issue_status_id option[value="4"]').attr('selected', 'selected');
-	$('#issue_assigned_to_id option:contains('+assigner+')').attr('selected', 'selected');
-	$("#issue_notes").val('Hi' + "\n" +'已完成 請確認 謝謝');
+	$('#issue_assigned_to_id option:contains(' + assigner + ')' ).attr('selected', 'selected');
+	$("#issue_notes").val('Hi ' + assigner + "\n" + '已完成 請確認 謝謝');
 	
 	// $("#issue-form").submit(); //submit will cause endless loop
 
